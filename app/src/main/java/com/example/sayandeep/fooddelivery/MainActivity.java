b0c8a6bb317b83630b9abe1d2490b774bfd39ef6
Sayandeep <sayandeepl63@gmail.com>
2018-04-19 17:47:54 +0530

    login

--- app/src/main/java/com/example/sayandeep/fooddelivery/MainActivity.java
+++ app/src/main/java/com/example/sayandeep/fooddelivery/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.sayandeep.fooddelivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=firebaseAuth.getCurrentUser();
+        if(currentUser==null){
+            Intent singUpIntent=new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(singUpIntent);
+        }
     }
 }
